8f3093322bb1bb36781fc694bdcaacb817a5b416
Mateusz <bmatteo@interia.pl>
2024-05-20 16:16:00 +0200

    update

--- src/main/java/com/comarch/szkolenia/rest/controllers/SimpleController.java
+++ src/main/java/com/comarch/szkolenia/rest/controllers/SimpleController.java
@@ -5,6 +5,8 @@ import com.comarch.szkolenia.rest.model.dto.CarsWrapper;
 import com.comarch.szkolenia.rest.services.CarService;
 import com.comarch.szkolenia.rest.services.ICarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -46,7 +48,10 @@ public class SimpleController {
 
     @PostMapping("/test5")
     public void test5(@RequestBody Car car) {
-
+        System.out.println(car.getId());
+        System.out.println(car.getBrand());
+        System.out.println(car.getModel());
+        System.out.println(car.getPlate());
     }
 
     @GetMapping("/test6")
@@ -54,5 +59,56 @@ public class SimpleController {
         CarsWrapper wrapper = new CarsWrapper();
         wrapper.setCars(this.carService.getCars());
         return wrapper;
+    }
+
+    @GetMapping("/test7")
+    public void test7(@RequestHeader("header1") String h1,
+                      @RequestHeader("header2") String h2) {
+        System.out.println(h1);
+        System.out.println(h2);
+    }
+
+    @PutMapping("/test8/{age}")
+    public Car test8(@RequestParam(name = "name", required = false) String imie,
+                     @RequestParam("surname") String nazwisko,
+                     @PathVariable(value = "age") int age,
+                     @RequestHeader(name = "header1", required = false) String h1,
+                     @RequestHeader("header2") String h2,
+                     @RequestBody(required = false) Car car) {
+        System.out.println(imie);
+        System.out.println(nazwisko);
+        System.out.println(age);
+        System.out.println(h1);
+        System.out.println(h2);
+        System.out.println(car.getId());
+        System.out.println(car.getBrand());
+        System.out.println(car.getModel());
+        System.out.println(car.getPlate());
+        return this.carService.getRandomCar();
+    }
+
+    @PutMapping("/test8/77")
+    public void cos() {
+
+    }
+
+    @GetMapping("/test9")
+    public ResponseEntity<Car> test9() {
+        Car car = this.carService.getRandomCar();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header("naglowek", "jakas wartosc naglowka")
+                .header("h2", "v2")
+                .body(car);
+    }
+
+    @PostMapping("/test10")
+    public Car test10(@RequestBody Car car) {
+        System.out.println("TEST10 !!!!");
+        car.setId(car.getId()+5);
+        car.setBrand(car.getBrand()+"X");
+        car.setModel(car.getModel()+"Y");
+        car.setPlate(car.getPlate()+"XYZ");
+
+        return car;
     }
 }
